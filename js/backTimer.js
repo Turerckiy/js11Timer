d65972b094e42a8c4a0864f696d58eb8e680b9a7
Tureckiy <ev.zhdanov@gmail.com>
2020-05-20 16:53:05 +0300

    11 edit

--- js/backTimer.js
+++ js/backTimer.js
@@ -1,45 +1,61 @@
-
 class CountdownTimer {
-  constructor({ selector, targetDate })  {
-  this.targetDate = targetDate;
-  this.selector = document.querySelector(`${selector}`);
-  this.days =  document.querySelector('[ data-value="days"]');
-  this.hours =  document.querySelector('[ data-value="hours"]');
-  this.mins =  document.querySelector('[ data-value="mins"]');
-  this.secs =  document.querySelector('[ data-value="secs"]');
-  this.firstTimer();
-  this.timer();
-  };
-    
+  constructor({ selector, targetDate, dateInput }) {
+    this.targetDate = targetDate;
+    this.selector = document.querySelector(`${selector}`);
+    this.days = document.querySelector('[ data-value="days"]');
+    this.hours = document.querySelector('[ data-value="hours"]');
+    this.mins = document.querySelector('[ data-value="mins"]');
+    this.secs = document.querySelector('[ data-value="secs"]');
+    this.input = dateInput;
+    this.firstTimer();
+    this.timer();
+  }
+
   firstTimer() {
     const timeDelta = this.targetDate.getTime() - Date.now();
     if (Date.now() >= this.targetDate.getTime()) {
-      this.calculation(0)
-      return
+      this.calculation(0);
+
+      return;
     }
     this.calculation(timeDelta);
   }
 
-timer() {
-  this.timerId = setInterval(() => {
-    if (Date.now() >= this.targetDate.getTime()) {
-      clearInterval(this.timerId);
-      return
+  timer() {
+    // console.log(this.input);
+    this.input = addEventListener("input", add);
+    function add() {
+      console.log("ww");
     }
-    this.firstTimer();
-  }, 1000);
-}
-calculation(timeDelta) {
-  this.days.textContent = this.pad(Math.floor(timeDelta / (1000 * 60 * 60 * 24)));
-  this.hours.textContent = this.pad(Math.floor((timeDelta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  this.mins.textContent = this.pad(Math.floor((timeDelta % (1000 * 60 * 60)) / (1000 * 60)));
-  this.secs.textContent = this.pad(Math.floor((timeDelta % (1000 * 60)) / 1000));
-}
+    this.timerId = setInterval(() => {
+      if (Date.now() >= this.targetDate.getTime()) {
+        clearInterval(this.timerId);
+        return;
+      }
+      this.firstTimer();
+    }, 1000);
+  }
+  calculation(timeDelta) {
+    const time = 1000 * 60 * 60 * 24; 
+    this.days.textContent = this.pad(
+      Math.floor(timeDelta / (time))
+    );
+    this.hours.textContent = this.pad(
+      Math.floor((timeDelta % (time)) / (1000 * 60 * 60))
+    );
+    this.mins.textContent = this.pad(
+      Math.floor((timeDelta % (time/24)) / (1000 * 60))
+    );
+    this.secs.textContent = this.pad(
+      Math.floor((timeDelta % (time/60/24)) / 1000)
+    );
+  }
 
-pad(value) {
-  return String(value).padStart(2, '0');
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
 }
-}
+{
 //  calcTime(timeDelta) {
 //   this.days.textContent = this.pad(Math.floor(timeDelta / (1000 * 60 * 60 * 24)));
 //   this.hours.textContent = this.pad(Math.floor((timeDelta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
@@ -54,16 +70,11 @@ pad(value) {
 // function pad(value) {
 //   return String(value).padStart(2, '0');
 //   }
-
-
-// '3'
-// https://github.com/goitacademy/javascript-homework/tree/master/homework-11/timer
-
-// Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
-
-
-// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
+}
 new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2020'),
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17,30049"),
+  dateInput: document.querySelector("dateInput"),
 });
+
+console.log(1000 * 60 * 60 * 24);
